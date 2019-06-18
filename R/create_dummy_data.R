874fd143060739ccf7cab25f8bb631d0ba34c90e
gghatano <adad0405@gmail.com>
2019-06-18 18:02:15 +0900

    transform

--- R/create_dummy_data.R
+++ R/create_dummy_data.R
@@ -1,7 +1,3 @@
-library(tidyverse)
-library(magrittr)
-library(stringi)
-library(openssl)
 
 ## ダミーデータを作る(マスター)
 create_dummy_master_data = function(people = 100){
@@ -9,11 +5,13 @@ create_dummy_master_data = function(people = 100){
   #'
   #' @param people 人数
   #'
-  #' @importFrom tidyverse
-  #' @importFrom magrittr
-  #' @importFrom stringi
-  #' @importFrom openssl
+  #' @importFrom tibble data_frame
+  #' @importFrom tibble as.tibble
+  #' @importFrom dplyr %>%
+  #' @importFrom stri_rand_strings
+  #' @importFrom openssl md5
   #' @export
+  #'
   #'
   ## エラー処理
   if(!is.numeric(people)){
@@ -26,27 +24,18 @@ create_dummy_master_data = function(people = 100){
   # 人数分のデータが生成される
   ROW_NUMBER = 1:people
   RAW_ID = ROW_NUMBER + 10000
-  ANON_ID = stri_rand_strings(n = people, length = 10)
   RAW_NUM = runif(n = people)
-  ANON_NUM = RAW_NUM + runif(n = people) * 0.01
   RAW_BIN = sample(x = c(0,1,100), prob = c(20,20,1), size = people, replace = TRUE)
-  ANON_BIN = RAW_BIN %>% if_else(. >= 100, 1, .)
-  RAW_CHAR = stri_rand_strings(n = people, length = 2)
-  ANON_CHAR = stri_rand_strings(n = people, length = 2)
+  RAW_CHAR = stringi::stri_rand_strings(n = people, length = 2)
 
-  dat_raw = data_frame(RAW_ROW_NUMBER = ROW_NUMBER,
-                        RAW_ID = RAW_ID,
-                        RAW_NUM = RAW_NUM,
-                        RAW_BIN = RAW_BIN,
-                        RAW_CHAR = RAW_CHAR)
+  dat_raw = tibble::data_frame(
+                        ROW_NUMBER = ROW_NUMBER,
+                        ID = RAW_ID,
+                        NUM = RAW_NUM,
+                        BIN = RAW_BIN,
+                        CHAR = RAW_CHAR)
 
-  dat_anon = data_frame(ANON_ROW_NUMBER = ROW_NUMBER,
-                        ANON_ID = ANON_ID,
-                        ANON_NUM = ANON_NUM,
-                        ANON_BIN = ANON_BIN,
-                        ANON_CHAR = ANON_CHAR)
-  # [dat_raw, dat_anon] %>% return
-  merge(dat_raw, dat_anon, all = TRUE) %>% as.tibble() %>% return
+  dat_raw %>% return
 }
 
 
@@ -57,7 +46,8 @@ create_dummy_transaction_data = function(people = 100, size = 2){
   #' @param people 人数
   #' @param size 平均トランザクション数
   #'
-  #' @importFrom tidyverse
+  #' @importFrom dplyr
+  #' @importFrom tibble
   #' @importFrom magrittr
   #' @importFrom stringi
   #' @importFrom openssl
@@ -65,8 +55,9 @@ create_dummy_transaction_data = function(people = 100, size = 2){
   #' data_tran = create_dummy_transaction_data(people = 10, size = 4)
   #' @export
 
-  if(!is.integer(people)){
-    stop("peopleには整数を指定してください")
+
+  if(!is.numeric(people)){
+    stop("peopleには数字を指定してください")
   } else if(!is.numeric(size)){
     stop("sizeには数値を指定してください")
   } else if(people < 1){
@@ -79,32 +70,24 @@ create_dummy_transaction_data = function(people = 100, size = 2){
 
   ROW_NUMBER = 1:row_num
   RAW_ID = sample(x = 1:people, size = row_num, replace = TRUE)
-  ANON_ID = md5(as.character(RAW_ID))
   RAW_NUM = runif(n = row_num)
-  ANON_NUM = RAW_NUM + runif(n = row_num) * 0.01
   RAW_BIN = sample(x = c(0,1,100), prob = c(20,20,1), size = row_num, replace = TRUE)
-  ANON_BIN = RAW_BIN %>% if_else(. >= 100, 1, .)
-  RAW_CHAR = stri_rand_strings(n = row_num, length = 2)
-  ANON_CHAR = RAW_CHAR
+  RAW_CHAR = stringi::stri_rand_strings(n = row_num, length = 2)
 
-  dat_raw = data_frame(RAW_ROW_NUMBER = ROW_NUMBER,
-                        RAW_ID = RAW_ID,
-                        RAW_NUM = RAW_NUM,
-                        RAW_BIN = RAW_BIN,
-                        RAW_CHAR = RAW_CHAR)
+  dat_raw = tibble::data_frame(
+                        ROW_NUMBER = ROW_NUMBER,
+                        ID = RAW_ID,
+                        NUM = RAW_NUM,
+                        BIN = RAW_BIN,
+                        CHAR = RAW_CHAR)
 
-  dat_anon = data_frame(ANON_ROW_NUMBER = ROW_NUMBER,
-                        ANON_ID = ANON_ID,
-                        ANON_NUM = ANON_NUM,
-                        ANON_BIN = ANON_BIN,
-                        ANON_CHAR = ANON_CHAR)
 
-  merge(dat_raw, dat_anon, all = TRUE) %>% as.tibble() %>% return
+  dat_raw %>% return
 }
 
 
 ## 再識別をしやすいように、加工前後のデータにヘッダを付けてクロスジョインする
-join_row_anon_data = function(raw, anon, raw_header = "RAW_", anon_header = "ANON_"){
+join_row_anon_data = function(raw, anon, raw_header = "RAW_", anon_header = "ANON_") {
   #' 再識別アルゴリズム適用に便利な形のデータを作る
   #'
   #' @param raw 匿名加工前データ(data.frame
@@ -112,11 +95,13 @@ join_row_anon_data = function(raw, anon, raw_header = "RAW_", anon_header = "ANO
   #' @param raw_header 加工前データの列の先頭に着けるラベル(デフォルトはRAW_)
   #' @param anon_header 加工後データの列の先頭に着けるラベル(デフォルトはAN_)
   #'
-  #' @importFrom tidyverse
+  #' @importFrom dplyr
+  #' @importFrom tibble
   #' @importFrom magrittr
   #' @importFrom stringi
   #' @importFrom openssl
   #' @export
+
 
   ## 引数のエラー処理
   if(is.data.frame(raw) + is.data.frame(anon) != 2){
@@ -128,7 +113,6 @@ join_row_anon_data = function(raw, anon, raw_header = "RAW_", anon_header = "ANO
   names(anon) = paste(anon_header, names(anon), sep = "")
 
   ## クロスジョインする
-  merge(raw, anon, all = TRUE) %>% as.tibble %>% return
-
+  merge(raw, anon, all = TRUE) %>% tibble::as.tibble %>% return
 }
 
---
+++ R/transform_transaction_to_master.R
@@ -0,0 +1,30 @@
+transform_transaction_to_master <- function(dat, ID = "ID", collapse = ":"){
+  #' トランザクションデータを無理やりマスタの形にする
+  #'
+  #' @param dat トランザクションデータ
+  #' @param ID  識別子の列名
+  #' @param collapse 圧縮時の区切り文字
+  #'
+  #' @importFrom dplyr
+  #' @importFrom magrittr
+  #' @export
+  #'
+
+  ## マスタへの変更
+  dat_master =
+    dat %>%
+    group_by(`ID`) %>%
+    summarise_all(~paste(sort(.), collapse = collapse))
+
+  ## 行数カウント
+  dat_rowcount =
+    dat %>%
+    group_by(`ID`) %>%
+    summarise(ROWCOUNT = n())
+
+  ## マージしてreturn
+  dat_master %>%
+    inner_join(dat_rowcount, by = ID) %>% return
+}
+
+
